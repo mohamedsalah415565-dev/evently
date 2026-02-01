4f540509dca7d2ef92c25ea8309e3c6cde854cb5
mohammed salah <mohamedsalah415565@gmail.com>
2026-02-01 17:11:17 +0200

    theme & welcome screen ui

---
+++ lib/app_theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primary = Color(0xff0E3A99);
+  static const Color primaryDark = Color(0xff457AED);
+  static const Color background = Color(0xffF4F7FF);
+  static const Color backgroundDark = Color(0xff000F30);
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xff1C1C1C);
+  static const Color gray = Color(0xff686868);
+  static const Color grayDark = Color(0xff000F30);
+
+  static ThemeData lightTheme = ThemeData();
+  static ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: background,
+
+    textTheme: TextTheme(
+      headlineMedium: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        color: primary,
+      ),
+      titleLarge: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: black,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+        color: gray,
+      ),
+    ),
+  );
+}
---
+++ lib/onboarding/welcome_screen.dart
@@ -0,0 +1,186 @@
+import 'package:evently_app/app_theme.dart';
+import 'package:evently_app/onboarding/onboarding.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String routeName = '/WelcomeScreen';
+
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  // Language: English / Arabic
+  final List<bool> languageSelected = [true, false];
+
+  // Theme: Light / Dark
+  final List<bool> themeSelected = [true, false];
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              /// Header Image
+              Image.asset('assets/images/headerlight.png'),
+
+              /// Welcome Image
+              Padding(
+                padding: EdgeInsets.only(top: 24),
+                child: Image.asset(
+                  'assets/images/welocome.png',
+                  height: 343,
+                  width: 343,
+                ),
+              ),
+
+              /// Title
+              Text(
+                'Personalize Your Experience',
+                style: textTheme.titleLarge!.copyWith(color: AppTheme.black),
+              ),
+
+              SizedBox(height: 8),
+
+              /// Description
+              Text(
+                'Choose your preferred theme and language to get started with a comfortable, tailored experience that suits your style.',
+                style: textTheme.titleMedium!.copyWith(color: AppTheme.gray),
+              ),
+
+              SizedBox(height: 16),
+
+              /// Language Row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Language',
+                    style: textTheme.headlineSmall!.copyWith(
+                      color: AppTheme.primary,
+                    ),
+                  ),
+                  ToggleButtons(
+                    isSelected: languageSelected,
+                    onPressed: (index) {
+                      setState(() {
+                        for (int i = 0; i < languageSelected.length; i++) {
+                          languageSelected[i] = i == index;
+                        }
+                      });
+                    },
+                    borderRadius: BorderRadius.circular(8),
+                    selectedColor: AppTheme.white,
+                    fillColor: AppTheme.primary,
+                    color: AppTheme.primary,
+                    constraints: const BoxConstraints(minHeight: 32),
+                    borderColor: Colors.transparent,
+
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 5,
+                        ),
+                        child: Text('English'),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 5,
+                        ),
+                        child: Text('Aarbic'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 16),
+
+              /// Theme Row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Theme',
+                    style: textTheme.headlineSmall!.copyWith(
+                      color: AppTheme.primary,
+                    ),
+                  ),
+                  ToggleButtons(
+                    isSelected: themeSelected,
+                    onPressed: (index) {
+                      setState(() {
+                        for (int i = 0; i < themeSelected.length; i++) {
+                          themeSelected[i] = i == index;
+                        }
+                      });
+                    },
+                    borderRadius: BorderRadius.circular(8),
+                    selectedColor: AppTheme.white,
+                    fillColor: AppTheme.primary,
+                    color: AppTheme.primary,
+                    constraints: BoxConstraints(minHeight: 32),
+                    borderColor: Colors.transparent,
+
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 5,
+                        ),
+                        child: Icon(Icons.light_mode),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 5,
+                        ),
+                        child: Icon(Icons.dark_mode),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
+              /// Get Started Button
+              Padding(
+                padding: const EdgeInsets.only(top: 24),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 9),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    foregroundColor: AppTheme.white,
+                    backgroundColor: AppTheme.primary,
+                  ),
+                  onPressed: () {
+                    Navigator.of(
+                      context,
+                    ).pushReplacementNamed(Onboarding.routeName);
+                  },
+                  child: Text(
+                    'Get Started',
+                    style: textTheme.titleLarge!.copyWith(
+                      color: AppTheme.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
