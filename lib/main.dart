96fbee7cd4f533338c9834c92c6ebb4005a23ab6
mohammed salah <mohamedsalah415565@gmail.com>
2026-03-04 14:36:39 +0200

    integrate with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,11 @@ import 'package:evently_app/onboarding/welcome_screen.dart';
 import 'package:evently_app/widgets/create_event_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   final prefs = await SharedPreferences.getInstance();
   final seenOnboarding = prefs.getBool('onboarding_completed') ?? false;
